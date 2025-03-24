1f5afb4f523b37cfd273dcb7347d869dcaaf8569
KMTonmoy <tonmoyahamed2009@gmail.com>
2025-03-24 07:27:41 +0600

    Customers Table Added

--- PostgreSQL_Assignment.sql
+++ PostgreSQL_Assignment.sql
@@ -1,4 +1,6 @@
 -- Active: 1742204523927@@127.0.0.1@5432@bookstore_db
+
+-- Create Books Table
 CREATE TABLE books (
     id SERIAL PRIMARY KEY,
     title VARCHAR(100) NOT NULL,
@@ -6,4 +8,12 @@ CREATE TABLE books (
     price NUMERIC(10, 2) CHECK (price >= 0) NOT NULL,
     stock INT CHECK (stock >= 0) NOT NULL,
     published_year INT CHECK (published_year >= 0) NOT NULL
+);
+
+-- Create customers Table 
+CREATE TABLE customers (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    email VARCHAR(100) UNIQUE NOT NULL,
+    joined_date DATE DEFAULT CURRENT_DATE
 );
